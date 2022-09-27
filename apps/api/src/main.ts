075b089b3a00d78072ffe7b49aa680b65c05416c
Zhenyu Shi <zhenyu.shi@compass.com>
2022-09-28 00:28:04 +0800

    add contentSecurityPolicy of helmet

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -6,6 +6,7 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter } from '@nestjs/platform-fastify';
+import helmet from 'helmet';
 
 import { AppModule } from './app/app.module';
 
@@ -13,6 +14,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new FastifyAdapter());
   const globalPrefix = 'graphql';
   const port = process.env.PORT || 3333;
+  const isProduction = process.env.NODE_ENV === 'production';
 
   // app.useGlobalPipes(
   //   new ValidationPipe({
@@ -27,27 +29,44 @@ async function bootstrap() {
   //   })
   // )
 
+  const developmentContentSecurityPolicy = {
+    directives: {
+      scriptSrc: [
+        "'self'",
+        "'unsafe-inline'",
+        "'unsafe-eval'",
+        'https://unpkg.com/',
+      ],
+    },
+  };
+
+  app.use(
+    helmet({
+      contentSecurityPolicy: isProduction
+        ? undefined
+        : developmentContentSecurityPolicy,
+    })
+  );
+
   app.enableCors({
-    origin: true
-  })
+    origin: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
       skipMissingProperties: true,
       //whitelist: true,
       transform: true,
-      transformOptions: { enableImplicitConversion: true }
+      transformOptions: { enableImplicitConversion: true },
     })
-  )
+  );
 
   await app.listen(port);
 
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/graphiql`
-  );
+  Logger.log(`🚀 Application is running on: http://localhost:${port}/graphiql`);
 }
 
 bootstrap();
